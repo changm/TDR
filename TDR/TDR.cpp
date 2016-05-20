03163bd9684057601d7d42b4141370799af4752c
Mason Chang <mchang@mozilla.com>
2016-05-20 16:51:46 -0700

    also enable devic enow

--- TDR/TDR.cpp
+++ TDR/TDR.cpp
@@ -70,5 +70,37 @@ int main() {
 			//printf("Couldn't call class installer: error: %lx\n", GetLastError());
 			wprintf(L"Error is: %s\n", errorString);
 		}
+
+		printf("Try to re-enable\n");
+		Sleep(1000 * 20);
+		memset(&changeParams, 0, sizeof(SP_PROPCHANGE_PARAMS));
+		changeParams.ClassInstallHeader.cbSize = sizeof(SP_CLASSINSTALL_HEADER);
+		changeParams.ClassInstallHeader.InstallFunction = DIF_PROPERTYCHANGE;
+		changeParams.Scope = DICS_FLAG_GLOBAL;
+		changeParams.StateChange = DICS_ENABLE;
+		results = SetupDiSetClassInstallParams(deviceInfo, &deviceInstance, (SP_CLASSINSTALL_HEADER*)&changeParams, sizeof(changeParams));
+		if (!results) {
+			printf("Couldn't setup class install params\n");
+			return 1;
+		}
+
+		results = SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, deviceInfo, &deviceInstance);
+		if (!results) {
+			DWORD error = GetLastError();
+			printf("Error code is: %llu \n", error);
+			HRESULT resultError = HRESULT_FROM_WIN32(error);
+			printf("HRESULT error is: %lu\n", resultError);
+			//LPSTR errorString = nullptr;
+			LPVOID errorString = nullptr;
+			FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+						  NULL,
+						  error,
+				MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), 
+				(LPSTR) errorString, 0, NULL);
+			//printf("Couldn't call class installer: error: %lx\n", GetLastError());
+			wprintf(L"Error is: %s\n", errorString);
+		}
+
+		printf("Re-enabled I hope\n");
 	}
 }
